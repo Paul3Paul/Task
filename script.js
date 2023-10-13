db5613975c9b2e307c14711391ca40a5456c6cea
Paul3Paul <144097486+Paul3Paul@users.noreply.github.com>
2023-10-14 01:31:41 +0530

    Update script.js

---
+++ script.js
@@ -0,0 +1,5 @@
+const showImageButton = document.getElementById("show-image-button");
+const myImage = document.getElementById("my-image"); 
+showImageButton.addEventListener("click", () => { 
+   myImage.style.display = "block"; 
+});
